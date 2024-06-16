4100704e40a4d6edb91ce4c05c5aedd922dc6aad
tom-elvidge <44055953+tom-elvidge@users.noreply.github.com>
2024-06-16 17:57:24 +0100

    added apple watch app

---
+++ src/racketreel_mobile/ios/RacketReel Watch App/RacketReelApp.swift
@@ -0,0 +1,17 @@
+//
+//  RacketReelApp.swift
+//  RacketReel Watch App
+//
+//  Created by Tom Elvidge on 16/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct RacketReel_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
