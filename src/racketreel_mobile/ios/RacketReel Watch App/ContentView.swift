a5cdb137a22f779f4fe2b0a7f97beb6d8a3e6592
tom-elvidge <44055953+tom-elvidge@users.noreply.github.com>
2024-07-13 19:30:48 +0100

    improve watch app

--- src/racketreel_mobile/ios/RacketReel Watch App/ContentView.swift
+++ src/racketreel_mobile/ios/RacketReel Watch App/ContentView.swift
@@ -2,71 +2,219 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var viewModel: WatchViewModel
-
+    
+    @State private var debounce_timer: Timer?
+    @State private var cancel_wait_timer: Timer?
+    
+    let helpItems = [
+        "Swipe up to score a point to team one",
+        "Swipe down to score a point to team two",
+        "Double tap to higlight the last point"
+    ]
+    
     var body: some View {
-        if (viewModel.hasState) {
+        if (viewModel.showQuitConfirmation) {
             VStack {
-                HStack {
-                    VStack {
-                        HStack {
-                            Text(viewModel.teamOneName)
-                            Image(systemName: "circle.fill")
-                                .font(.system(size: 6))
-                                .opacity(viewModel.isTeamOneServing ? 1 : 0)
-                        }
-                        HStack {
-                            Text(viewModel.teamTwoName)
-                            Image(systemName: "circle.fill")
-                                .font(.system(size: 6))
-                                .opacity(viewModel.isTeamOneServing ? 0 : 1)
-                        }
-                    }
-                    // Sets
-                    VStack {
-                        Text(viewModel.teamOneSets)
-                        Text(viewModel.teamTwoSets)
-                    }
-                    // Games
-                    VStack {
-                        Text(viewModel.teamOneGames)
-                        Text(viewModel.teamTwoGames)
-                    }
-                    // Points
-                    VStack {
-                        Text(viewModel.teamOnePoints)
-                        Text(viewModel.teamTwoPoints)
-                    }
-                }
-                Button(action: {
-                    viewModel.pointToTeamOne()
-                }) {
-                    Text("Point to \(viewModel.teamOneName)")
-                }
-                
-                Button(action: {
-                    viewModel.pointToTeamTwo()
-                }) {
-                    Text("Point to \(viewModel.teamTwoName)")
-                }
-                Button(action: {
-                    viewModel.undoLastPoint()
-                }) {
-                    Text("Undo")
-                }
-                Button(action: {
-                    viewModel.toggleHighlight()
-                }) {
-                    Text(viewModel.lastStateHighlighted ? "Remove Highlight" : "Highlight")
-                }
-            }
-            .overlay(alignment: .topLeading) {
-                Image(systemName: viewModel.lastStateHighlighted ? "heart.fill" : "heart")
-            }
-        } else {
-            VStack{
-                Text("Start a match from your phone")
+                Text("Stop scoring?")
+                    .multilineTextAlignment(.center)
                     .padding()
+                Button(action: { viewModel.quit()}) {
+                    Text("Yes")
+                }
+                Button(action: { viewModel.toggleShowQuitConfirmation()}) {
+                    Text("No")
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        } else if (viewModel.showHelp) {
+            ScrollView{
+                VStack(alignment: .leading) {
+                    ForEach(helpItems, id: \.self) { item in
+                        HStack {
+                            Image(systemName: "circle.fill")
+                                .font(.system(size: 8))
+                                .padding(.trailing, 4)
+                            Text(item)
+                        }
+                        .padding(.bottom, 2)
+                    }
+                    Button(action: { viewModel.toggleShowHelp() }) {
+                        Text("Done")
+                    }
+                    .padding(.top, 4)
+                    Button(action: { viewModel.isRecordingWorkout
+                        ? viewModel.endWorkout()
+                        : viewModel.startWorkout() }) {
+                        Text(viewModel.isRecordingWorkout ? "End workout" : "Start workout")
+                    }
+                }
             }
         }
+        else if (viewModel.hasState) {
+            VStack(alignment: .center) {
+                if (viewModel.lastStateHighlighted) {
+                    HStack {
+                        Image(systemName: "heart.fill")
+                            .font(.system(size: 10))
+                        Text("Point highlighted")
+                            .font(.system(size: 12))
+                    }
+                }
+                if (viewModel.isRecordingWorkout) {
+                    HStack {
+                        Image(systemName: "waveform.path.ecg")
+                            .font(.system(size: 10))
+                        Text("Recording workout")
+                            .font(.system(size: 12))
+                    }
+                }
+                Spacer()
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.black)
+                    .stroke(Color.white, lineWidth: 1)
+                    .frame(height: 60)
+                    .padding()
+                    .overlay(
+                        HStack {
+                            // Serving indicator
+                            VStack {
+                                Image(systemName: "tennisball.fill")
+                                    .font(.system(size: 10))
+                                    .opacity(viewModel.isTeamOneServing ? 1 : 0)
+                                    .frame(height: 20)
+                                Image(systemName: "tennisball.fill")
+                                    .font(.system(size: 10))
+                                    .opacity(viewModel.isTeamOneServing ? 0 : 1)
+                                    .frame(height: 20)
+                            }
+                            // Names
+                            VStack (alignment: .leading) {
+                                Text(viewModel.teamOneName)
+                                    .font(.system(size: 14))
+                                    .frame(height: 20)
+                                Text(viewModel.teamTwoName)
+                                    .font(.system(size: 14))
+                                    .frame(height: 20)
+                            }
+                            Spacer()
+                            // Sets
+                            VStack {
+                                Text(viewModel.teamOneSets)
+                                    .frame(width: 15, height: 20)
+                                Text(viewModel.teamTwoSets)
+                                    .frame(width: 15, height: 20)
+                            }
+                            // Games
+                            VStack {
+                                Text(viewModel.teamOneGames)
+                                    .frame(width: 15, height: 20)
+                                Text(viewModel.teamTwoGames)
+                                    .frame(width: 15, height: 20)
+                            }
+                            // Points
+                            VStack {
+                                Text(viewModel.teamOnePoints)
+                                    .frame(width: 25, height: 20)
+                                Text(viewModel.teamTwoPoints)
+                                    .frame(width: 25, height: 20)
+                            }
+                        }
+                            .padding(14)
+                    )
+                Spacer()
+                HStack {
+                    Button(action: { viewModel.toggleShowQuitConfirmation() }) {
+                        Image(systemName: "xmark")
+                    }
+                    Button(action: { viewModel.undoLastPoint() }) {
+                        Image(systemName: "arrow.uturn.backward")
+                    }
+                    Button(action: { viewModel.toggleShowHelp() }) {
+                        Image(systemName: "questionmark")
+                    }
+                }
+                    .padding()
+                    .padding(.horizontal)
+            }
+                .overlay(
+                    Group {
+                        if (viewModel.isWaitingForState) {
+                            Color.black.opacity(0.4)
+                                .edgesIgnoringSafeArea(.all)
+                                .overlay(
+                                    VStack {
+                                        ProgressView()
+                                            .padding()
+                                            .cornerRadius(10)
+                                            .shadow(radius: 5)
+                                    }
+                                )
+                                // only show the waiting indicator for max 10s
+                                .onAppear{
+                                    cancel_wait_timer?.invalidate()
+                                    cancel_wait_timer = Timer.scheduledTimer(withTimeInterval: 10, repeats: false) { _ in
+                                        viewModel.stopWaitingForState()
+                                    }
+                                }
+                        }
+                    }
+                )
+                .edgesIgnoringSafeArea(.bottom)
+                .background(Color.black)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            debounce_timer?.invalidate()
+                            debounce_timer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { _ in
+                                if (value.translation.height > (WKInterfaceDevice.current().screenBounds.height / 4)) {
+                                    WKInterfaceDevice.current().play(.click)
+                                    viewModel.pointToTeamTwo()
+                                }
+                                
+                                if (value.translation.height < -(WKInterfaceDevice.current().screenBounds.height / 4)) {
+                                    WKInterfaceDevice.current().play(.click)
+                                    viewModel.pointToTeamOne()
+                                }
+                            }
+                        }
+                )
+                .onTapGesture(count: 2) {
+                    WKInterfaceDevice.current().play(.click)
+                    viewModel.toggleHighlight()
+                }
+        } else {
+            VStack{
+                Text("Create a match from your phone")
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = WatchViewModel()
+        viewModel.hasState = true;
+        viewModel.teamOneName = "Team A"
+        viewModel.teamTwoName = "Team B"
+        viewModel.teamOnePoints = "30"
+        viewModel.teamTwoPoints = "15"
+        viewModel.teamOneGames = "2"
+        viewModel.teamTwoGames = "1"
+        viewModel.teamOneSets = "1"
+        viewModel.teamTwoSets = "0"
+        viewModel.isTeamOneServing = true
+        viewModel.lastStateHighlighted = true
+        viewModel.hasState = true
+        viewModel.isWaitingForState = false
+        viewModel.showHelp = false
+        viewModel.showQuitConfirmation = false
+        viewModel.isRecordingWorkout = true
+        
+        return ContentView()
+            .environmentObject(viewModel)
     }
 }
--- src/racketreel_mobile/ios/RacketReel Watch App/WatchViewModel.swift
+++ src/racketreel_mobile/ios/RacketReel Watch App/WatchViewModel.swift
@@ -1,5 +1,6 @@
 import Foundation
 import WatchConnectivity
+import HealthKit
 
 class WatchViewModel: NSObject, ObservableObject, WCSessionDelegate {
     @Published var hasState = false;
@@ -13,6 +14,14 @@ class WatchViewModel: NSObject, ObservableObject, WCSessionDelegate {
     @Published var teamTwoSets = "";
     @Published var isTeamOneServing = false;
     @Published var lastStateHighlighted = false;
+    @Published var showHelp = false;
+    @Published var showQuitConfirmation = false;
+    @Published var isWaitingForState = false;
+    @Published var isRecordingWorkout = false;
+    
+    
+    var healthStore: HKHealthStore?
+    var workoutSession: HKWorkoutSession?
 
     override init() {
         super.init()
@@ -45,6 +54,7 @@ class WatchViewModel: NSObject, ObservableObject, WCSessionDelegate {
                 self.isTeamOneServing = message["serving"] as! String == "1";
                 self.lastStateHighlighted = message["lastStateHighlighted"] as! String == "true";
                 self.hasState = true;
+                self.isWaitingForState = false;
             } catch {
                 self.teamOneName = "";
                 self.teamTwoName = "";
@@ -57,6 +67,7 @@ class WatchViewModel: NSObject, ObservableObject, WCSessionDelegate {
                 self.isTeamOneServing = false;
                 self.lastStateHighlighted = false;
                 self.hasState = false;
+                self.isWaitingForState = false;
             }
         }
     }
@@ -67,6 +78,7 @@ class WatchViewModel: NSObject, ObservableObject, WCSessionDelegate {
             WCSession.default.sendMessage(message, replyHandler: nil) { (error) in
                 print("Error sending message: \(error.localizedDescription)")
             }
+            self.isWaitingForState = true;
         }
     }
     
@@ -76,6 +88,7 @@ class WatchViewModel: NSObject, ObservableObject, WCSessionDelegate {
             WCSession.default.sendMessage(message, replyHandler: nil) { (error) in
                 print("Error sending message: \(error.localizedDescription)")
             }
+            self.isWaitingForState = true;
         }
     }
     
@@ -85,6 +98,7 @@ class WatchViewModel: NSObject, ObservableObject, WCSessionDelegate {
             WCSession.default.sendMessage(message, replyHandler: nil) { (error) in
                 print("Error sending message: \(error.localizedDescription)")
             }
+            self.isWaitingForState = true;
         }
     }
     
@@ -94,6 +108,95 @@ class WatchViewModel: NSObject, ObservableObject, WCSessionDelegate {
             WCSession.default.sendMessage(message, replyHandler: nil) { (error) in
                 print("Error sending message: \(error.localizedDescription)")
             }
+            self.isWaitingForState = true;
         }
+    }
+    
+    func toggleShowHelp() {
+        showHelp = !showHelp
+    }
+    
+    func toggleShowQuitConfirmation() {
+        showQuitConfirmation = !showQuitConfirmation
+    }
+    
+    func quit() {
+        self.teamOneName = "";
+        self.teamTwoName = "";
+        self.teamOnePoints = "";
+        self.teamTwoPoints = "";
+        self.teamOneGames = "";
+        self.teamTwoGames = "";
+        self.teamOneSets = "";
+        self.teamTwoSets = "";
+        self.isTeamOneServing = false;
+        self.lastStateHighlighted = false;
+        self.hasState = false;
+        self.showQuitConfirmation = false;
+        self.showHelp = false;
+        
+        endWorkout()
+    }
+    
+    func stopWaitingForState() {
+        isWaitingForState = false;
+    }
+    
+    func startWorkout() {
+        // Request access to write health data
+        if (self.healthStore == nil && HKHealthStore.isHealthDataAvailable()) {
+            self.healthStore = HKHealthStore()
+
+            self.healthStore!.requestAuthorization(toShare: [HKQuantityType.workoutType()], read: nil) { (success, error) in
+                guard success else {
+                    return
+                }
+            }
+        }
+        
+        let configuration = HKWorkoutConfiguration()
+        configuration.activityType = .tennis
+        configuration.locationType = .unknown
+        
+        do {
+            self.workoutSession = try HKWorkoutSession(healthStore: self.healthStore!, configuration: configuration)
+            let builder = self.workoutSession!.associatedWorkoutBuilder()
+            builder.dataSource = HKLiveWorkoutDataSource(healthStore: healthStore!, workoutConfiguration: configuration)
+            
+            self.workoutSession!.startActivity(with: Date())
+            builder.beginCollection(withStart: Date()) { (success, error) in
+                guard success else {
+                    return
+                }
+            }
+        } catch {
+            print("Cannot start workout")
+            return
+        }
+        
+        isRecordingWorkout = true
+    }
+    
+    func endWorkout() {
+        if (workoutSession == nil) {
+            isRecordingWorkout = false
+            return;
+        }
+        
+        self.workoutSession!.end()
+        let builder = self.workoutSession!.associatedWorkoutBuilder()
+        builder.endCollection(withEnd: Date()) { (success, error) in
+            guard success else {
+                return
+            }
+            
+            builder.finishWorkout { (workout, error) in
+                guard workout != nil else {
+                    return
+                }
+            }
+        }
+        
+        isRecordingWorkout = false
     }
 }
